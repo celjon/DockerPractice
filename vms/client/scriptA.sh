58b968a7fe3bb7627df97cc1dde3139a6b3655dd
celjon <134550766+celjon@users.noreply.github.com>
2023-06-04 18:33:41 +0300

    Delete scriptA.sh

--- vms/client/scriptA.sh
+++
@@ -1,7 +0,0 @@
-ip link add macvlan1 link eth0 type macvlan mode bridge # создаем адаптер тип bridge
-ip address add dev macvlan1 192.168.28.10/24 # добавляем ему ip
-ip link set macvlan1 up # включаем 
-
-ip route add 192.168.7.0/24 via 192.168.28.1 # маршрут к вм С через вм В
-
-docker-compose up -d
